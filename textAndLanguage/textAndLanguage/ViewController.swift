38f42078faefe8e1d306ea57857e41d393b56a1c
alessonJuley <alessonabao@gmail.com>
2023-03-12 21:06:23 +1300

    added text and language project folder

---
+++ textAndLanguage/textAndLanguage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  textAndLanguage
+//
+//  Created by Alesson Abao on 12/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
